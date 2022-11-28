d6080332cd161a50c4cc95cf8cbc229cbdc2dc13
fitch <metneg@gmail.com>
2022-11-28 10:40:52 +0100

    chore(contracts) update contracts address

--- constants/tokens.js
+++ constants/tokens.js
@@ -2,22 +2,22 @@
 exports.__esModule = true;
 exports.ESDS = exports.ESD = exports.USDC = exports.UNI = void 0;
 exports.UNI = {
-    addr: "0xae13dff69d84d87ae50ad841377d5f9d33d857cf",
+    addr: "0xa2f1E7E65bfdBD264D6e3cB4dA50eDF7fc77b861",
     decimals: 18,
     symbol: "Op3CRV-f"
 };
 exports.USDC = {
-    addr: "0x1337BedC9D22ecbe766dF105c9623922A27963EC",
+    addr: "0x1337bedc9d22ecbe766df105c9623922a27963ec",
     decimals: 18,
     symbol: "3CRV"
 };
 exports.ESD = {
-    addr: "0x85b0b1aF13aCa01b7b5eDbE5BFbBeC2e130D2227",
+    addr: "0x018A1e566C0847e35e7D46e4ee0428c35574a3D8",
     decimals: 18,
     symbol: "T"
 };
 exports.ESDS = {
-    addr: "0xF767DB17601EA3e011546Dec8c04ff38FaDDB222",
+    addr: "0x524215604FACFd6820fdD5b7CFA4232238A47DaF",
     decimals: 18,
     symbol: "TS"
 };
